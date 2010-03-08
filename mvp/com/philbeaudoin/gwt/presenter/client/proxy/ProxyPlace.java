5027eb0fce306a3d9c36dc94e6d1ec8e89803433
philippe.beaudoin <philippe.beaudoin@localhost>
2010-03-08 23:14:19 +0000

    Corrected a small bug with the previous revision.

--- mvp/com/philbeaudoin/gwt/presenter/client/proxy/ProxyPlace.java
+++ mvp/com/philbeaudoin/gwt/presenter/client/proxy/ProxyPlace.java
@@ -1,28 +1,26 @@
 package com.philbeaudoin.gwt.presenter.client.proxy;
 
-public interface ProxyPlace extends Proxy, Place {
+import com.philbeaudoin.gwt.presenter.client.EventBus;
+import com.philbeaudoin.gwt.presenter.client.Presenter;
 
-  /**
-   * Requests the presenter to reveal itself on screen.
-   * Upon being revealed presenters will ask to be inserted within 
-   * their parent presenters by firing a {@link SetContentEvent}
-   * which will cause the parent to be revealed too.
-   */
-  public void reveal();
+/**
+ * A useful mixing class to define a {@link Proxy} that is also
+ * a {@link Place}. See {@link ProxyPlaceAbstract} for more details.
+ * 
+ * @param <P> Type of the associated {@link Presenter}.
+ *
+ * @author David Peterson
+ * @author Philippe Beaudoin
+ */
+public class ProxyPlace<P extends Presenter> 
+extends ProxyPlaceAbstract<P, Proxy<P>> {
 
-  /**
-   * Checks if the associated presenter can be revealed.
-   * <p />
-   * The default implementation of this method always return
-   * <code>true</code>, but subclasses should override this and
-   * check to make sure the current user has the privileges
-   * to see the place. Make sure the places you request in 
-   * {@link PlaceManager#revealDefaultPlace()} and
-   * {@link PlaceManager#revealErrorPlace(String)} can 
-   * reveal themselves, otherwise your application could get into
-   * an infinite loop.
-   *
-   * @return <code>true</code> if the presenter can be revealed, <code>false</code> otherwise.
-   */
-  public boolean canReveal();
+  public ProxyPlace(
+      final EventBus eventBus, 
+      final PlaceManager placeManager,
+      final Proxy<P> proxy, 
+      final Place place) {
+    super(eventBus, placeManager, proxy, place);
+  }
+
 }
