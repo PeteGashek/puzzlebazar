25efe8872624a079ec354f0d37e8a4acf415add6
philippe.beaudoin <philippe.beaudoin@localhost>
2010-03-11 17:19:29 +0000

    Complete - task 38: Wait until login is confirmed before refusing to navigate http://code.google.com/p/puzzlebazar/issues/detail?id=38

--- src/com/puzzlebazar/client/PuzzlebazarPlaceManager.java
+++ src/com/puzzlebazar/client/PuzzlebazarPlaceManager.java
@@ -1,6 +1,8 @@
 package com.puzzlebazar.client;
 
 import com.google.gwt.inject.client.AsyncProvider;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Timer;
 import com.google.inject.Inject;
 import com.philbeaudoin.gwt.presenter.client.EventBus;
 import com.philbeaudoin.gwt.presenter.client.proxy.Callback;
@@ -18,18 +20,36 @@ import com.puzzlebazar.client.resources.Translations;
 public class PuzzlebazarPlaceManager extends PlaceManagerImpl {
 
   private final CallbackProvider<ProxyBase> defaultProxy;
+  private final CurrentUser currentUser;
+  private final Timer retryTimer;
+  private final int retryDelay;
+  private final int maxRetries;
+  private int nbRetry = 0;
 
   @Inject
   public PuzzlebazarPlaceManager(
       final EventBus eventBus, 
       final TokenFormatter tokenFormatter,
       @DefaultPlace final AsyncProvider<ProxyBase> defaultProxy,
+      final CurrentUser currentUser,
       final Translations translations ) {
     super(eventBus, tokenFormatter);
-    
+
     this.defaultProxy = new CodeSplitProvider<ProxyBase>(defaultProxy, translations);
+    this.currentUser = currentUser;
+    // TODO These should be injected when GIN supports toInstance injection
+    this.retryDelay = 500;    
+    this.maxRetries = 4;
+
+    this.retryTimer = new Timer(){
+      @Override
+      public void run() {
+        History.fireCurrentHistoryState();
+      }    
+    };
+
   }
-  
+
   @Override
   public void revealDefaultPlace() {
     defaultProxy.get( new Callback<ProxyBase>(){
@@ -38,6 +58,28 @@ public class PuzzlebazarPlaceManager extends PlaceManagerImpl {
         proxy.reveal();
       }
     } );
+  }
+
+  /**
+   * This method will usually reveal the default place. When the application
+   * launches, however, it is possible that the error comes from the fact
+   * that the user hasn't had time to load yet (the asynchronous call to
+   * the server hasn't yet returned). In this case, we wait a while and
+   * if the user logs-in the meantime, we re-run the request.
+   * 
+   * @param invalidHistoryToken The history token that was not recognised.
+   *        It will be retried if the user logs in.
+   * 
+   * @see PlaceManagerImpl#revealErrorPlace(String)
+   */
+  @Override
+  public void revealErrorPlace( String invalidHistoryToken ) {
+    if( !currentUser.isConfirmed() && nbRetry < maxRetries ) {
+      nbRetry++;
+      retryTimer.schedule( retryDelay );
+    } 
+    else
+      revealDefaultPlace();
   }
 
 }
---
+++ src/com/puzzlebazar/client/core/presenter/CurrentUserChangedEvent.java
@@ -0,0 +1,51 @@
+package com.puzzlebazar.client.core.presenter;
+
+import com.google.gwt.event.shared.GwtEvent;
+import com.philbeaudoin.gwt.presenter.client.EventBus;
+import com.puzzlebazar.shared.model.User;
+
+/**
+ * This event is sent to the {@link EventBus} when the current user
+ * information has changed. For example, if the user logged in or
+ * logged out.
+ *
+ * @author Philippe Beaudoin
+ */
+public class CurrentUserChangedEvent extends GwtEvent<CurrentUserChangedHandler> {
+
+    private static final Type<CurrentUserChangedHandler> TYPE = new Type<CurrentUserChangedHandler>();
+
+    public static Type<CurrentUserChangedHandler> getType() {
+        return TYPE;
+    }
+
+    public static void fire( EventBus eventBus, User userInfo ) {
+        eventBus.fireEvent( new CurrentUserChangedEvent( userInfo ) );
+    }
+
+    private final User currentUser;
+
+    public CurrentUserChangedEvent( User currentUser ) {
+        this.currentUser = currentUser;
+    }
+
+    /**
+     * Access the current user attached to this event.
+     * 
+     * @return The {@link User} object of the current user, 
+     *         or <code>null</code> if user is not logged in.
+     */
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    @Override
+    protected void dispatch( CurrentUserChangedHandler handler ) {
+        handler.onCurrentUserChanged( this );
+    }
+
+    @Override
+    public Type<CurrentUserChangedHandler> getAssociatedType() {
+        return getType();
+    }
+}
