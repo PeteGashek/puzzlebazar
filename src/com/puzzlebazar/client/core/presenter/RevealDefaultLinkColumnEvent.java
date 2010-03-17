9959ff4a5fa79696e015d9497281cb4e7703c894
philippe.beaudoin <philippe.beaudoin@localhost>
2010-03-17 23:43:53 +0000

    Incomplete - task 11: Port puzzle editing http://code.google.com/p/puzzlebazar/issues/detail?id=11
    Corrected issue with link column.

---
+++ src/com/puzzlebazar/client/core/presenter/RevealDefaultLinkColumnEvent.java
@@ -0,0 +1,32 @@
+package com.puzzlebazar.client.core.presenter;
+
+import com.google.gwt.event.shared.GwtEvent;
+import com.philbeaudoin.platform.mvp.client.EventBus;
+
+
+public class RevealDefaultLinkColumnEvent extends GwtEvent<RevealDefaultLinkColumnHandler> {
+
+  private static final Type<RevealDefaultLinkColumnHandler> TYPE = new Type<RevealDefaultLinkColumnHandler>();
+  
+  public static Type<RevealDefaultLinkColumnHandler> getType() {
+      return TYPE;
+  }
+
+  public static void fire( EventBus eventBus ) {
+      eventBus.fireEvent( new RevealDefaultLinkColumnEvent() );
+  }
+  
+  public RevealDefaultLinkColumnEvent() {
+  }
+
+  @Override
+  protected void dispatch( RevealDefaultLinkColumnHandler handler ) {
+    handler.onRevealDefaultLinkColumn( this );
+  }
+
+  @Override
+  public Type<RevealDefaultLinkColumnHandler> getAssociatedType() {
+    return getType();
+  }
+
+}
---
+++ src/com/puzzlebazar/client/core/presenter/RevealDefaultLinkColumnHandler.java
@@ -0,0 +1,9 @@
+package com.puzzlebazar.client.core.presenter;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface RevealDefaultLinkColumnHandler extends EventHandler {
+
+  void onRevealDefaultLinkColumn( RevealDefaultLinkColumnEvent event );
+
+}
