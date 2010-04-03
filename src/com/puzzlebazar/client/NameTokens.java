f4cc7c5a1e6eba5e4e2724198c3e2dfa18a05493
philippe.beaudoin <philippe.beaudoin@localhost>
2010-04-03 23:11:31 +0000

    Complete - task 100: Add Apache License Header http://code.google.com/p/puzzlebazar/issues/detail?id=100

--- src/com/puzzlebazar/client/NameTokens.java
+++ src/com/puzzlebazar/client/NameTokens.java
@@ -1,6 +1,22 @@
 package com.puzzlebazar.client;
 
 /**
+ * Copyright 2010 Philippe Beaudoin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * The central location of all name tokens for the application.
  * All {@link ProxyPlace} classes get their tokens from here.
  * This class also makes it easy to use name tokens as
